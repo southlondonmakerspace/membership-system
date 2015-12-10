223e5f96bfbaa4adcc5894fad8bafa51e9fb5d34
Tom Lynch <me@unknowndomain.co.uk>
2015-12-10 23:40:08 +0000

    Add dummy support for Passport.js/Persona testing

--- src/js/app.js
+++ src/js/app.js
@@ -11,6 +11,27 @@ var	express = require( 'express' ),
 	app = express(),
 	http = require( 'http' ).Server( app );
 
+var passport = require( 'passport' ),
+	PersonaStrategy = require( 'passport-persona' ).Strategy;
+
+// Add support for persona authentication
+passport.use( new PersonaStrategy( { audience: 'http://localhost:3001' },
+	function( email, done ) {
+	    process.nextTick(function () {
+	    	if ( config.users.indexOf( email ) != -1 )
+				return done( null, { email: email }, { message: 'User login successful' } );
+			return done( null, false, { message: 'Unauthorised user' } );
+		} );
+	}
+) );
+
+passport.serializeUser( function( user, done ) {
+	done( null, user.email );
+} );
+
+passport.deserializeUser( function( email, done ) {
+	done(null, { email: email });
+} );
 
 // Setup static route
 app.use( express.static( __dirname + '../../../static' ) );
@@ -31,6 +52,10 @@ app.use( session( {
 
 // Include support for notifications
 app.use( flash() );
+
+// Include support for passport and sessions
+app.use( passport.initialize() );
+app.use( passport.session() );
 
 // Load in local variables such as config.globals
 app.use( function( req, res, next ) {

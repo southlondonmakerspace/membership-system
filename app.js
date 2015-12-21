a50066c0296f3598af75304d524cf02ec7c96e90
Tom Lynch <me@unknowndomain.co.uk>
2015-12-21 17:52:51 +0000

    Change path of app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 "use strict";
 
 var body = require( 'body-parser' ),
-	config = require( '../../config/config.json' ),
-	database = require( './database'),
+	config = require( __dirname + '/config/config.json' ),
+	database = require( __dirname + '/src/js/database'),
 	express = require( 'express' ),
 	flash = require( 'express-flash' ),
 	swig = require( 'swig'),
@@ -12,17 +12,17 @@ var body = require( 'body-parser' ),
 
 
 // handle authentication
-require( './authentication' )( app );
+require( __dirname + '/src/js/authentication' )( app );
 
 // Setup static route
-app.use( express.static( __dirname + '../../../static' ) );
+app.use( express.static( __dirname + '/static' ) );
 
 // Enable support for form post data
 app.use( body.json() );
 app.use( body.urlencoded( { extended: true } ) );
 
 // handle sessions
-require( './sessions' )( app );
+require( __dirname + '/src/js/sessions' )( app );
 
 // Include support for notifications
 app.use( flash() );
@@ -31,9 +31,6 @@ app.use( function( req, res, next ) {
 	var flash = req.flash(),
 		flashes = [],
 		types = Object.keys( flash );
-
-		console.log( flash );
-		console.log( types );
 
 	for ( var t in types ) {
 		var key = types[ t ];
@@ -52,19 +49,20 @@ app.use( function( req, res, next ) {
 
 // Load in local variables such as config.globals
 app.use( function( req, res, next ) {
+	res.locals.apps = config.apps;
 	res.locals.config = config.globals;
 	next();
 } );
 
 // Use SWIG to render pages
 app.engine( 'swig', swig.renderFile );
-app.set( 'views', __dirname + './../views' );
+app.set( 'views', __dirname + '/src/views' );
 app.set( 'view engine', 'swig' );
 app.set( 'view cache', false ); // Disables cache
 swig.setDefaults( { cache: false } ); // Disables cache
 
 // Generic routes
-require( './routes' )( app );
+require( __dirname + '/src/js/routes' )( app );
 
 // Start server
 app.listen( config.port );

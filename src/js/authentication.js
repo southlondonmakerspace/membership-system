7147d1deca2c9560bcd88135482dfc29dd795b5f
Tom Lynch <me@unknowndomain.co.uk>
2015-12-21 21:39:49 +0000

    Switching to database authentication.
    To add your username for basic testing, in the terminal type:
    mongo
    use membership-system
    db.members.insert( { email: ‘name@example.com’ } );
    exit

--- src/js/authentication.js
+++ src/js/authentication.js
@@ -3,6 +3,7 @@
 "use strict";
 
 var config = require( '../../config/config.json' ),
+	database = require( './database'),
 	passport = require( 'passport' ),
 	PersonaStrategy = require( 'passport-persona' ).Strategy;
 
@@ -11,18 +12,18 @@ module.exports =  function( app ) {
 	// Add support for persona authentication
 	passport.use( new PersonaStrategy( { audience: config.audience },
 		function( email, done ) {
-
 			process.nextTick( function () {
-
-				if ( config.users.indexOf( email ) != -1 ){
-
-					return done( null, { email: email }, { message: 'User login successful' } );
-				}
-				return done( null, false, { message: 'Unauthorised user' } );
+				database.Members.findOne( { email: email }, function( err, user ) {
+					if ( user != null ) {
+						console.log( user );
+						return done( null, { email: email }, { message: 'User login successful' } );
+					} else {
+						return done( null, false, { message: 'Unauthorised user' } );
+					}
+				} );
 			} );
 		}
 	) );
-
 
 	passport.serializeUser( function( user, done ) {
 		done( null, user.email );

cdc25f403a89be0b2dd093998382395c917de388
Tom Lynch <me@unknowndomain.co.uk>
2015-12-23 21:50:36 +0000

    Adds support for basic profile editing without validation

--- apps/profile/app.js
+++ apps/profile/app.js
@@ -2,12 +2,32 @@
 
 var	express = require( 'express' ),
 	app = express(),
-	passport = require( 'passport' );
+	passport = require( 'passport' ),
+	Members = require( '../../src/js/database' ).Members;
 
 app.set( 'views', __dirname + '/views' );
 
 app.get( '/', ensureAuthenticated, function( req, res ) {
-	res.render( 'index', { user: req.user } );
+	res.render( 'profile', { user: req.user } );
+} );
+
+app.get( '/update', ensureAuthenticated, function( req, res ) {
+	res.render( 'update', { user: req.user } );
+} );
+
+app.post( '/update', ensureAuthenticated, function( req, res ) {
+	var profile = {
+		firstname: req.body.firstname,
+		lastname: req.body.lastname,
+		email: req.body.email,
+		tag_id: req.body.tag_id,
+		address: req.body.address
+	};
+
+	Members.update( { _id: req.user._id }, { $set: profile }, function( status ) {
+		req.flash( 'success', 'Your profile has been updated' );
+		res.redirect( '/profile' );
+	} );
 } );
 
 module.exports = app;
--- src/js/database.js
+++ src/js/database.js
@@ -10,66 +10,24 @@ exports.connect = function( url ) {
 
 var memberSchema = mongoose.Schema( {
 	firstname: {
-		type: String
+		type: String,
+		required: true
 	},
 	lastname: {
-		type: String
-	},
-	uuid: {
-		type: String ,
-		default: function () { // pseudo uuid4
-			function s4() {
-				return Math.floor( ( 1 + Math.random() ) * 0x10000 ).toString( 16 ).substring( 1 );
-			};
-			return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
-		},
-		unique: true
+		type: String,
+		required: true
 	},
 	email: {
-		type: String
+		type: String,
+		required: true
 	},
 	address: {
+		type: String,
+		required: true
+	},
+	tag_id: {
 		type: String
-	}, // users physical address
-	card_id: {
-		type: String
-	}, // the ID of the NFC card
-	card_id_hashed: {
-		type: String
-	}, // the MD5 hash of the card id with salt
-	disabled: {
-		type: Boolean,
-		default: false
-	}, // for when the user wants to disable their account (self)
-	approved: {
-		type: Boolean,
-		default: true
-	}, // used for when an an administrator wishes to disable the account (admin)
-	gc_subscription: {
-		type: String
-	}, // for when a GoCardless subscription has been set up
-	gc_donation: {
-		type: String
-	}, // for when a GoCardless subscription has been set up
-	last_payment: {
-		type: Date
-	}, // when the last payment was received
-	membership_expires: {
-		type: Date
-	}, // when the last payment was received
-	joined: {
-		type: Date,
-		default: function () { return new Date; }
-	}, // when the account was created (not their first payment)
-	last_accessed: {
-		type: Date
-	}, // when they last accessed the website
-	last_entered: {
-		type: Date
-	}, // last recorded that they went in to the space
-	last_updated: {
-		type: Date
-	} // last time any entry was updated
+	}
 } );
 
 memberSchema.virtual( 'fullname' ).get( function() {

39c34b1e1dbd43cbd2a6b55e1320183a28248d42
Tom Lynch <me@unknowndomain.co.uk>
2015-12-25 18:29:25 +0000

    Adding code to send activation email from Mandrill

--- src/js/routes.js
+++ src/js/routes.js
@@ -2,10 +2,17 @@
 
 var	express = require( 'express' ),
 	app = express(),
-	passport = require( 'passport' ),
-	Members = require( './database' ).Members,
-	ObjectId = require( 'mongoose' ).Schema.Types.ObjectId,
-	crypto = require( 'crypto' );
+	passport = require( 'passport' );
+
+var Members = require( './database' ).Members,
+	ObjectId = require( 'mongoose' ).Schema.Types.ObjectId;
+
+var crypto = require( 'crypto' );
+
+var config = require( '../../config/config.json' );
+
+var mandrill = require( 'mandrill-api/mandrill' ),
+	mandrill_client = new mandrill.Mandrill( config.mandrill.api_key );
 
 app.set( 'views', __dirname + '/../views' );
 
@@ -95,8 +102,40 @@ app.post( '/join', function( req, res ) {
 							req.session.join = user;
 							res.redirect( '/join' );
 						} else {
-							req.flash( 'success', 'Account created, please check your email for a registration link' );
-							res.redirect( '/' );
+							var message = {
+								subject: 'Activation Email – ' + config.globals.organisation,
+								from_email: config.mandrill.from_email,
+								from_name: config.mandrill.from_name,
+								to: [ {
+									email: user.email,
+									name: user.firstname + ' ' + user.lastname,
+								} ],
+								track_opens: true,
+								track_clicks: true,
+								global_merge_vars: [
+									{
+										name: 'NAME',
+										content: user.firstname
+									},
+									{
+										name: 'LINK',
+										content: config.audience + '/activate/' + user.activation_code
+									}
+								]
+							};
+
+							mandrill_client.messages.sendTemplate( {
+								template_name: 'activation-email',
+								template_content: null,
+								message: message
+							}, function ( e ) {
+								req.flash( 'success', 'Account created, please check your email for a registration link' );
+								res.redirect( '/' );
+							}, function ( e ) {
+								req.flash( 'danger', 'Your account was created, but there was a problem sending the activation email, please contact: ' + config.mandrill.from_name );
+								res.redirect( '/' );
+								console.log( e );
+							} );
 							// Send an email
 						}
 					} );

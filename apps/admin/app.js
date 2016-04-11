5f601f42c2a80f59f204c9bd325cad4aa04a71d4
unknowndomain <me@unknowndomain.co.uk>
2016-04-11 01:37:31 +0100

    Error checking for invalid IDs

--- apps/admin/app.js
+++ apps/admin/app.js
@@ -26,106 +26,179 @@ app.get( '/', auth.isAdmin, function( req, res ) {
  *	MEMBERS
  */
 
-var members = express();
-members.set( 'views', __dirname + '/views' );
+	var members = express();
+	members.set( 'views', __dirname + '/views' );
 
-members.use( function( req, res, next ) {
-	res.locals.breadcrumb.push( {
-		name: "Members",
-		url: "/admin/members"
+	members.use( function( req, res, next ) {
+		res.locals.breadcrumb.push( {
+			name: "Members",
+			url: "/admin/members"
+		} );
+		next();
 	} );
-	next();
-} );
 
-members.get( '/', auth.isAdmin, function( req, res ) {
-	Members.find( function( err, members ) {
-		res.render( 'members', { members: members } );
+	members.get( '/', auth.isAdmin, function( req, res ) {
+		Members.find( function( err, members ) {
+			res.render( 'members', { members: members } );
+		} );
 	} );
-} );
 
-members.get( '/:id/edit', auth.isAdmin, function( req, res ) {
-	Members.findOne( { _id: req.params.id }, function( err, member ) {
-		if ( member != undefined ) {
+	members.get( '/:id/edit', auth.isAdmin, function( req, res ) {
+		Members.findOne( { _id: req.params.id }, function( err, member ) {
+			if ( member == undefined ) {
+				req.flash( 'warning', 'Member not found' );
+				res.redirect( '/admin/members' );
+				return;
+			}
 			res.locals.breadcrumb.push( {
 				name: member.fullname
 			} );
 			res.render( 'edit-member', { member: member } );
-		} else {
-			res.render( '../../../src/views/404' );
-		}
+		} );
 	} );
-} );
 
-members.post( '/:id/edit', auth.isAdmin, function( req, res ) {
-	var member = {
-		firstname: req.body.firstname,
-		lastname: req.body.lastname,
-		email: req.body.email,
-		tag_id: req.body.tag_id,
-		address: req.body.address
-	};
+	members.post( '/:id/edit', auth.isAdmin, function( req, res ) {
+		var member = {
+			firstname: req.body.firstname,
+			lastname: req.body.lastname,
+			email: req.body.email,
+			tag_id: req.body.tag_id,
+			address: req.body.address
+		};
 
-	Members.update( { _id: req.params.id }, member, function( status ) {
-		req.flash( 'success', 'Members updated' );
-		res.redirect( '/admin/members/' + req.params.id + '/edit' );
+		Members.update( { _id: req.params.id }, member, function( status ) {
+			req.flash( 'success', 'Members updated' );
+			res.redirect( '/admin/members/' + req.params.id + '/edit' );
+		} );
 	} );
-} );
 
-members.get( '/:id/permissions', auth.isAdmin, function( req, res ) {
-	Permissions.find( function( err, permissions ) {
-		Members.findOne( { _id: req.params.id } ).populate( 'permissions.permission' ).exec( function( err, member ) {
+	members.get( '/:id/permissions', auth.isAdmin, function( req, res ) {
+		Permissions.find( function( err, permissions ) {
+			Members.findOne( { _id: req.params.id } ).populate( 'permissions.permission' ).exec( function( err, member ) {
+				if ( member == undefined ) {
+					req.flash( 'warning', 'Member not found' );
+					res.redirect( '/admin/members' );
+					return;
+				}
+
+				res.locals.breadcrumb.push( {
+					name: member.fullname,
+					url: '/admin/members/' + member._id + '/edit'
+				} );
+				res.locals.breadcrumb.push( {
+					name: 'Permissions'
+				} );
+				res.render( 'member-permissions', { permissions: permissions, member: member, now: new Date() } );
+			} );
+		} );
+	} );
+
+	members.post( '/:id/permissions', auth.isAdmin, function( req, res ) {
+		Permissions.findOne( { slug: req.body.permission }, function( err, permission ) {
+			if ( permission != undefined ) {
+				var new_permission = {
+					permission: permission._id
+				}
+
+				new_permission.date_added = new Date( req.body.start_date + 'T' + req.body.start_time );
+
+				if ( req.body.expiry_date != '' && req.body.expiry_time != '' )
+					new_permission.date_expires = new Date( req.body.expiry_date + 'T' + req.body.expiry_time );
+
+				if ( new_permission.date_added >= new_permission.date_expires ) {
+					req.flash( 'warning', 'Expiry date must not be the same as or before the start date' );
+					res.redirect( '/admin/members/' + req.params.id + '/permissions' );
+					return;
+				}
+
+				Members.update( { _id: req.params.id }, {
+					$push: {
+						permissions: new_permission
+					}
+				}, function ( status ) {
+				} );
+			} else {
+				req.flash( 'warning', 'Invalid permission selected' );
+			}
+			res.redirect( '/admin/members/' + req.params.id + '/permissions' );
+		} );
+	} );
+
+	members.get( '/:mid/permissions/:pid/modify', auth.isAdmin, function( req, res ) {
+		Permissions.find( function( err, permissions ) {
+			Members.findOne( { _id: req.params.mid } ).populate( 'permissions.permission' ).exec( function( err, member ) {
+				if ( member == undefined ) {
+					req.flash( 'warning', 'Member not found' );
+					res.redirect( '/admin/members' );
+					return;
+				}
+				
+				if ( member.permissions.id( req.params.pid ) == undefined ) {
+					req.flash( 'warning', 'Permission not found' );
+					res.redirect( '/admin/members' );
+					return;
+				}
+
+				res.locals.breadcrumb.push( {
+					name: member.fullname,
+					url: '/admin/members/' + member._id + '/edit'
+				} );
+				res.locals.breadcrumb.push( {
+					name: 'Permissions',
+					url: '/admin/members/' + member._id + '/permissions'
+				} );
+				res.locals.breadcrumb.push( {
+					name: member.permissions.id( req.params.pid ).permission.name
+				} );
+				res.render( 'edit-member-permission', { permissions: permissions, member: member, current: member.permissions.id( req.params.pid ) } );
+			} );
+		} );
+	} );
+
+	members.post( '/:mid/permissions/:pid/modify', auth.isAdmin, function( req, res ) {
+		Members.findOne( { _id: req.params.mid }, function( err, member ) {
 			if ( member == undefined ) {
 				req.flash( 'warning', 'Member not found' );
 				res.redirect( '/admin/members' );
 				return;
 			}
 
-			res.locals.breadcrumb.push( {
-				name: member.fullname,
-				url: '/admin/members/' + member._id + '/edit'
-			} );
-			res.locals.breadcrumb.push( {
-				name: 'Permissions'
-			} );
-			res.render( 'member-permissions', { permissions: permissions, member: member, now: new Date() } );
-		} );
-	} );
-} );
-
-members.post( '/:id/permissions', auth.isAdmin, function( req, res ) {
-	Permissions.findOne( { slug: req.body.permission }, function( err, permission ) {
-		if ( permission != undefined ) {
-			var new_permission = {
-				permission: permission._id
-			}
-
-			new_permission.date_added = new Date( req.body.start_date + 'T' + req.body.start_time );
-
-			if ( req.body.expiry_date != '' && req.body.expiry_time != '' )
-				new_permission.date_expires = new Date( req.body.expiry_date + 'T' + req.body.expiry_time );
-
-			if ( new_permission.date_added >= new_permission.date_expires ) {
-				req.flash( 'warning', 'Expiry date must not be the same as or before the start date' );
-				res.redirect( '/admin/members/' + req.params.id + '/permissions' );
+			if ( member.permissions.id( req.params.pid ) == undefined ) {
+				req.flash( 'warning', 'Permission not found' );
+				res.redirect( '/admin/members' );
 				return;
 			}
 
-			Members.update( { _id: req.params.id }, {
-				$push: {
-					permissions: new_permission
+			Permissions.findOne( { slug: req.body.permission }, function( err, newPermission ) {
+				if ( newPermission == undefined ) {
+					req.flash( 'warning', 'New permission not found' );
+					res.redirect( '/admin/members' );
+					return;
 				}
-			}, function ( status ) {
-			} );
-		} else {
-			req.flash( 'warning', 'Invalid permission selected' );
-		}
-		res.redirect( '/admin/members/' + req.params.id + '/permissions' );
-	} );
-} );
 
-members.get( '/:mid/permissions/:pid/modify', auth.isAdmin, function( req, res ) {
-	Permissions.find( function( err, permissions ) {
-		Members.findOne( { _id: req.params.mid } ).populate( 'permissions.permission' ).exec( function( err, member ) {
+				var permission = member.permissions.id( req.params.pid );
+				permission.permission = newPermission._id;
+				permission.date_added = new Date( req.body.start_date + 'T' + req.body.start_time );
+
+				if ( req.body.expiry_date != '' && req.body.expiry_time != '' )
+					permission.date_expires = new Date( req.body.expiry_date + 'T' + req.body.expiry_time );
+
+				if ( permission.date_added >= permission.date_expires ) {
+					req.flash( 'warning', 'Expiry date must not be the same as or before the start date' );
+					res.redirect( '/admin/members/' + req.params.mid + '/permissions' );
+					return;
+				}
+
+				member.save( function ( err ) {
+					req.flash( 'success', 'Permission updated' );
+					res.redirect( '/admin/members/' + req.params.mid + '/permissions' );
+				} );
+			} );
+		} );
+	} );
+
+	members.get( '/:mid/permissions/:pid/revoke', auth.isAdmin, function( req, res ) {
+		Members.findOne( { _id: req.params.mid }, function( err, member ) {
 			if ( member == undefined ) {
 				req.flash( 'warning', 'Member not found' );
 				res.redirect( '/admin/members' );
@@ -138,161 +211,95 @@ members.get( '/:mid/permissions/:pid/modify', auth.isAdmin, function( req, res )
 				return;
 			}
 
-			res.locals.breadcrumb.push( {
-				name: member.fullname,
-				url: '/admin/members/' + member._id + '/edit'
-			} );
-			res.locals.breadcrumb.push( {
-				name: 'Permissions',
-				url: '/admin/members/' + member._id + '/permissions'
-			} );
-			res.locals.breadcrumb.push( {
-				name: member.permissions.id( req.params.pid ).permission.name
-			} );
-			res.render( 'edit-member-permission', { permissions: permissions, member: member, current: member.permissions.id( req.params.pid ) } );
-		} );
-	} );
-} );
-
-members.post( '/:mid/permissions/:pid/modify', auth.isAdmin, function( req, res ) {
-	Members.findOne( { _id: req.params.mid }, function( err, member ) {
-		if ( member == undefined ) {
-			req.flash( 'warning', 'Member not found' );
-			res.redirect( '/admin/members' );
-			return;
-		}
-
-		if ( member.permissions.id( req.params.pid ) == undefined ) {
-			req.flash( 'warning', 'Permission not found' );
-			res.redirect( '/admin/members' );
-			return;
-		}
-
-		Permissions.findOne( { slug: req.body.permission }, function( err, newPermission ) {
-			if ( newPermission == undefined ) {
-				req.flash( 'warning', 'New permission not found' );
-				res.redirect( '/admin/members' );
-				return;
-			}
-
-			var permission = member.permissions.id( req.params.pid );
-			permission.permission = newPermission._id;
-			permission.date_added = new Date( req.body.start_date + 'T' + req.body.start_time );
-
-			if ( req.body.expiry_date != '' && req.body.expiry_time != '' )
-				permission.date_expires = new Date( req.body.expiry_date + 'T' + req.body.expiry_time );
-
-			if ( permission.date_added >= permission.date_expires ) {
-				req.flash( 'warning', 'Expiry date must not be the same as or before the start date' );
-				res.redirect( '/admin/members/' + req.params.mid + '/permissions' );
-				return;
-			}
+			member.permissions.pull( { _id: req.params.pid } );
 
 			member.save( function ( err ) {
-				req.flash( 'success', 'Permission updated' );
+				req.flash( 'success', 'Permission removed' );
 				res.redirect( '/admin/members/' + req.params.mid + '/permissions' );
 			} );
 		} );
 	} );
-} );
 
-members.get( '/:mid/permissions/:pid/revoke', auth.isAdmin, function( req, res ) {
-	Members.findOne( { _id: req.params.mid }, function( err, member ) {
-		if ( member == undefined ) {
-			req.flash( 'warning', 'Member not found' );
-			res.redirect( '/admin/members' );
-			return;
-		}
-		
-		if ( member.permissions.id( req.params.pid ) == undefined ) {
-			req.flash( 'warning', 'Permission not found' );
-			res.redirect( '/admin/members' );
-			return;
-		}
-
-		member.permissions.pull( { _id: req.params.pid } );
-
-		member.save( function ( err ) {
-			req.flash( 'success', 'Permission removed' );
-			res.redirect( '/admin/members/' + req.params.mid + '/permissions' );
-		} );
-	} );
-} );
-
-app.use( '/members', members );
+	app.use( '/members', members );
 
 /*
  *	PERMISSIONS
  */
 
-var permissions = express();
-permissions.set( 'views', __dirname + '/views' );
+	var permissions = express();
+	permissions.set( 'views', __dirname + '/views' );
 
-permissions.use( function( req, res, next ) {
-	res.locals.breadcrumb.push( {
-		name: "Permissions",
-		url: "/admin/permissions"
-	} );
-	next();
-} );
-
-permissions.get( '/', auth.isAdmin, function( req, res ) {
-	Permissions.find( function( err, permissions ) {
-		res.render( 'permissions', { permissions: permissions } );
-	} );
-} );
-
-permissions.get( '/create', auth.isAdmin, function( req, res ) {
-	res.locals.breadcrumb.push( {
-		name: 'Create'
-	} );
-	res.render( 'create-permission' );
-} );
-
-permissions.post( '/create', auth.isAdmin, function( req, res ) {
-	var permission = {
-		name: req.body.name,
-		slug: req.body.slug,
-		description: req.body.description
-	};
-
-	new Permissions( permission ).save( function( err, permission ) {
-		req.flash( 'success', 'Permission created' );
-		res.redirect( '/admin/permissions/' + permission._id + '/edit' );
-	} );
-} );
-
-permissions.get( '/:id/edit', auth.isAdmin, function( req, res ) {
-	Permissions.findOne( { _id: req.params.id }, function( err, permission ) {
+	permissions.use( function( req, res, next ) {
 		res.locals.breadcrumb.push( {
-			name: permission.name
+			name: "Permissions",
+			url: "/admin/permissions"
 		} );
-		res.render( 'edit-permission', { permission: permission } );
+		next();
 	} );
-} );
 
-permissions.post( '/:id/edit', auth.isAdmin, function( req, res ) {
-	var permission = {
-		name: req.body.name,
-		slug: req.body.slug,
-		description: req.body.description
-	};
-
-	Permissions.update( { _id: req.params.id }, permission, function( status ) {
-		req.flash( 'success', 'Permission updated' );
-		res.redirect( '/admin/permissions/' + req.params.id + '/edit' );
+	permissions.get( '/', auth.isAdmin, function( req, res ) {
+		Permissions.find( function( err, permissions ) {
+			res.render( 'permissions', { permissions: permissions } );
+		} );
 	} );
-} );
 
-app.use( '/permissions', permissions );
+	permissions.get( '/create', auth.isAdmin, function( req, res ) {
+		res.locals.breadcrumb.push( {
+			name: 'Create'
+		} );
+		res.render( 'create-permission' );
+	} );
+
+	permissions.post( '/create', auth.isAdmin, function( req, res ) {
+		var permission = {
+			name: req.body.name,
+			slug: req.body.slug,
+			description: req.body.description
+		};
+
+		new Permissions( permission ).save( function( err, permission ) {
+			req.flash( 'success', 'Permission created' );
+			res.redirect( '/admin/permissions/' + permission._id + '/edit' );
+		} );
+	} );
+
+	permissions.get( '/:id/edit', auth.isAdmin, function( req, res ) {
+		Permissions.findOne( { _id: req.params.id }, function( err, permission ) {
+			if ( permission == undefined ) {
+				req.flash( 'warning', 'Permission not found' );
+				res.redirect( '/admin/permissions' );
+				return;
+			}
+		
+			res.locals.breadcrumb.push( {
+				name: permission.name
+			} );
+			res.render( 'edit-permission', { permission: permission } );
+		} );
+	} );
+
+	permissions.post( '/:id/edit', auth.isAdmin, function( req, res ) {
+		var permission = {
+			name: req.body.name,
+			slug: req.body.slug,
+			description: req.body.description
+		};
+
+		Permissions.update( { _id: req.params.id }, permission, function( status ) {
+			req.flash( 'success', 'Permission updated' );
+			res.redirect( '/admin/permissions/' + req.params.id + '/edit' );
+		} );
+	} );
+
+	app.use( '/permissions', permissions );
 
 /*
  *	SETTINGS
  */
 
- app.get( '/settings', auth.isAdmin, function( req, res ) {
- 	req.flash( 'info', 'This area has not yet been built.' );
- 	res.redirect( '/admin' );
- } );
+	 app.get( '/settings', auth.isAdmin, function( req, res ) {
+	 	req.flash( 'info', 'This feature has not yet been implemented.' );
+	 	res.redirect( '/admin' );
+	 } );
 
 module.exports = app;

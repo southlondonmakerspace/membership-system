fcc1079fa278093c203f21ce81be4d24bd7b2a16
unknowndomain <me@unknowndomain.co.uk>
2016-04-16 21:21:25 +0100

    Creating basic stub statement app

---
+++ apps/statement/app.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var	express = require( 'express' ),
+	app = express();
+
+var auth = require( '../../src/js/authentication.js' ),
+	Members = require( '../../src/js/database' ).Members;
+
+app.set( 'views', __dirname + '/views' );
+
+app.use( function( req, res, next ) {
+	res.locals.breadcrumb.push( {
+		name: "Statement",
+		url: "/statement"
+	} );
+	res.locals.activeApp = 'statement';
+	next();
+} );
+
+app.get( '/', auth.isMember, function( req, res ) {
+	var transactions = [];
+	res.render( 'index', { transactions: req.user.gocardless.transactions } );
+} );
+
+module.exports = app;

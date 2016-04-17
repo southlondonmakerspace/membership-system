965a2e57407983e4c8fb70097f99adc3002545e0
unknowndomain <me@unknowndomain.co.uk>
2016-04-17 16:18:02 +0100

    Add API endpoint for access control

--- apps/api/app.js
+++ apps/api/app.js
@@ -3,10 +3,40 @@
 var	express = require( 'express' ),
 	app = express();
 
+var Members = require( '../../src/js/database' ).Members;
+
 app.get( '/', function( req, res ) {
 	var response = [ 'API not yet implemented' ];
 	res.setHeader( 'Content-Type', 'application/json' );
 	res.send( JSON.stringify( response ) );
+} );
+
+app.get( '/permission/:slug/:tag', function( req, res ) {
+	Members.findOne( { tag_hashed: req.params.tag } ).populate( 'permissions.permission' ).exec( function( err, member ) {
+		var grantAccess = false;
+
+		if ( member != undefined ) {
+			var hasMembership = false;
+			var hasPermission = false;
+			var isTrustee = false;
+
+			for ( var p = 0; p < member.permissions.length; p++ ) {
+				var permission = member.permissions[p];
+				if ( permission.permission.slug == 'trustee' && permission.date_added <= new Date() && ( permission.date_expires == undefined || permission.date_expires > new Date() ) ) isTrustee = true;
+				if ( permission.permission.slug == 'member' && permission.date_added <= new Date() && ( permission.date_expires == undefined || permission.date_expires > new Date() ) ) hasMembership = true;
+				if ( permission.permission.slug == req.params.slug && permission.date_added <= new Date() && ( permission.date_expires == undefined || permission.date_expires > new Date() ) ) hasPermission = true;
+			}
+
+			if ( isTrustee || ( hasMembership && hasPermission ) )
+				grantAccess = true;
+		}
+
+		if ( grantAccess ) {
+			res.sendStatus( 200 );
+		} else {
+			res.sendStatus( 403 );
+		}
+	} );
 } );
 
 module.exports = app;

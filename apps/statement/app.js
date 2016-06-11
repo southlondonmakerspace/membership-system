ec3686b0ab1de951a2da9faa57374339f0d1f4ce
Tom Lynch <me@unknowndomain.co.uk>
2016-06-11 23:23:30 +0100

    Including new payment statuses

--- apps/statement/app.js
+++ apps/statement/app.js
@@ -27,7 +27,6 @@ app.use( function( req, res, next ) {
 
 app.get( '/', auth.isMember, function( req, res ) {
 	Payments.find( { member: req.user._id }, function( err, payments ) {
-		console.log( payments );
 		res.render( 'index', { payments: payments } );
 	} );
 } );

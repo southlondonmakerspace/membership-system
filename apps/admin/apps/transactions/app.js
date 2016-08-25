b80c7c45cd093803add56499b2a136e5b2631153
unknowndomain <me@unknowndomain.co.uk>
2016-08-25 16:24:15 +0100

    Adding a transactions admin app

---
+++ apps/admin/apps/transactions/app.js
@@ -0,0 +1,45 @@
+"use strict";
+
+var __root = '../../../..';
+var __src = __root + '/src';
+var __js = __src + '/js';
+var __config = __root + '/config';
+
+var	express = require( 'express' ),
+	app = express(),
+	formBodyParser = require( 'body-parser' ).urlencoded( { extended: true } );
+
+var Payments = require( __js + '/database' ).Payments,
+	Members = require( __js + '/database' ).Members;
+
+var auth = require( __js + '/authentication' );
+
+var messages = require( __src + '/messages.json' );
+
+var config = require( __config + '/config.json' );
+
+var app_config = {};
+
+app.set( 'views', __dirname + '/views' );
+
+app.use( function( req, res, next ) {
+	res.locals.app = app_config;
+	res.locals.breadcrumb.push( {
+		name: app_config.title,
+		url: app.parent.mountpath + app.mountpath
+	} );
+	res.locals.activeApp = app_config.uid;
+	next();
+} );
+
+app.get( '/', auth.isAdmin, function( req, res ) {
+	Payments.find().populate( 'member' ).exec( function( err, payments ) {
+		res.render( 'transactions', { payments: payments } );
+		console.log( payments[0].member );
+	} );
+} );
+
+module.exports = function( config ) {
+	app_config = config;
+	return app;
+};

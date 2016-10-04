7c92c645d7a675bfd8774930a5cb7c0958d2648c
unknowndomain <me@unknowndomain.co.uk>
2016-10-04 19:08:01 +0100

    Moving permissions into a separate app

---
+++ apps/permissions/app.js
@@ -0,0 +1,53 @@
+"use strict";
+
+var __root = '../..';
+var __src = __root + '/src';
+var __js = __src + '/js';
+var __config = __root + '/config';
+
+var	express = require( 'express' ),
+	app = express();
+
+var auth = require( __js + '/authentication' ),
+	discourse = require( __js + '/discourse' ),
+	Permissions = require( __js + '/database' ).Permissions,
+	Members = require( __js + '/database' ).Members;
+
+var messages = require( __src + '/messages.json' );
+
+var config = require( __config + '/config.json' );
+
+var app_config = {};
+
+app.set( 'views', __dirname + '/views' );
+
+app.use( function( req, res, next ) {
+	res.locals.app = app_config;
+	res.locals.breadcrumb.push( {
+		name: app_config.title,
+		url: app.mountpath
+	} );
+	res.locals.activeApp = app_config.uid;
+	next();
+} );
+
+app.get( '/', auth.isMember, function( req, res ) {
+	Permissions.find( function( err, permissions ) {
+		for ( var p = 0; p < permissions.length; p++ ) {
+			var permission = permissions[p];
+			if ( req.user.quickPermissions.indexOf( permission.slug ) != -1 )
+				permission.granted = true;
+		}
+		res.render( 'permissions', { permissions: permissions } );
+	} );
+} );
+
+app.get( '/:uuid', auth.isMember, function( req, res ) {
+
+} );
+
+
+module.exports = function( config ) {
+	app_config = config;
+	return app;
+};

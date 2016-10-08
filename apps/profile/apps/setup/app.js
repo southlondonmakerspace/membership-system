c50ab8f14795a3e41c37f580c4d21cc7951e104a
unknowndomain <me@unknowndomain.co.uk>
2016-10-08 22:19:13 +0100

    Create Setup app

---
+++ apps/profile/apps/setup/app.js
@@ -0,0 +1,43 @@
+"use strict";
+
+var __root = '../../../..';
+var __src = __root + '/src';
+var __js = __src + '/js';
+var __config = __root + '/config';
+
+var	express = require( 'express' ),
+	app = express();
+
+var auth = require( __js + '/authentication' ),
+	discourse = require( __js + '/discourse' ),
+	Permissions = require( __js + '/database' ).Permissions,
+	Members = require( __js + '/database' ).Members;
+
+var messages = require( __src + '/messages.json' );
+
+var config = require( __config + '/config.json' );
+
+var app_config = {};
+
+app.set( 'views', __dirname + '/views' );
+
+app.use( function( req, res, next ) {
+	res.locals.app = app_config;
+	res.locals.breadcrumb.push( {
+		name: app_config.title,
+		url: app.mountpath
+	} );
+	res.locals.activeApp = app_config.uid;
+	next();
+} );
+
+app.get( '/', auth.isLoggedIn, function( req, res ) {
+	Members.findById( req.user._id, function( err, user ) {
+		res.render( 'setup', { user: user } );
+	} )
+} );
+
+module.exports = function( config ) {
+	app_config = config;
+	return app;
+};

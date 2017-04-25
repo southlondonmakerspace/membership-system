a045334fbec9960958cadcf3bdf60cb71f994ad8
unknowndomain <me@unknowndomain.co.uk>
2017-04-25 22:02:37 +0100

    Abstract app loading into a separate file.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var __config = __dirname + '/config/config.json';
 var __static = __dirname + '/static';
 var __src = __dirname + '/src';
-var __apps = __dirname + '/apps';
 var __views = __src + '/views';
 var __js = __src + '/js';
 
@@ -12,12 +11,10 @@ var config = require( __config ),
 	flash = require( 'express-flash' ),
 	app = express(),
 	http = require( 'http' ).Server( app ),
-	fs = require( 'fs' );
+	app_loader = require( __js + '/app-loader.js' );
 
 // Use helmet
 app.use( helmet() );
-
-var apps = [];
 
 console.log( "Starting..." );
 
@@ -34,83 +31,13 @@ require( __js + '/sessions' )( app );
 app.use( flash() );
 app.use( require( __js + '/quickflash' ) );
 
-// Load apps
-
-// Loop through main app director contents
-var files = fs.readdirSync( __apps );
-for ( var f in files ) {
-
-	// Only read directories
-	var file = __apps + '/' + files[f];
-	if ( fs.statSync( file ).isDirectory() ) {
-
-		// Check for a config.json file
-		var config_file = file + '/config.json';
-		if ( fs.existsSync( config_file ) ) {
-
-			// Parse the config into apps array
-			var output = JSON.parse( fs.readFileSync( config_file ) );
-			output.uid = files[f];
-			if ( output.priority === undefined ) output.priority = 100;
-			output.app = file + '/app.js';
-
-			// Check for sub apps directory
-			output.subapps = [];
-			var subapp_path = file + '/apps';
-			if ( fs.existsSync( subapp_path ) ) {
-				// Fetch the contents of the subapp directory
-				var subapps = fs.readdirSync( subapp_path );
-				for ( var a in subapps ) {
-					// Only read directories
-					var subapp = subapp_path + '/' + subapps[a];
-					if ( fs.statSync( subapp ).isDirectory() ) {
-						// Check for a config.json file
-						var sub_config_file = subapp + '/config.json';
-						if ( fs.existsSync( sub_config_file ) ) {
-							// Parse the config into apps array
-							var subapp_output = JSON.parse( fs.readFileSync ( sub_config_file ) );
-							subapp_output.uid = subapps[a];
-							if ( subapp_output.priority === undefined ) subapp_output.priority = 100;
-							subapp_output.app = subapp + '/app.js';
-							output.subapps.push( subapp_output );
-						}
-					}
-				}
-			}
-			output.subapps.sort( function( a, b ) {
-				return a.priority < b.priority;
-			} );
-			apps.push( output );
-		}
-	}
-}
-apps.sort( function( a, b ) {
-	return a.priority < b.priority;
-} );
-
-// Load in local variables such as config.globals
-app.use( require( __js + '/template-locals' )( apps ) );
-
 // Use PUG to render pages
 app.set( 'views', __views );
 app.set( 'view engine', 'pug' );
 app.set( 'view cache', false );
 
-// Route apps
-for ( var a in apps ) {
-	var _app = apps[a];
-	console.log( "	Route: /" + _app.path );
-	var new_app = require( _app.app )( _app );
-	app.use( '/' + _app.path, new_app );
-	if ( _app.subapps.length > 0 ) {
-		for ( var s in _app.subapps ) {
-			var _sapp = _app.subapps[s];
-			console.log( "	       /" + _app.path + "/" + _sapp.path  );
-			var new_sub_app = require( _sapp.app )( _sapp );
-			new_app.use( '/' + _sapp.path, new_sub_app );
-		}
-	}
-}
+// Load apps
+app_loader( app );
 
 // Error 404
 app.get( '*', function( req, res ) {
---
+++ src/js/app-loader.js
@@ -0,0 +1,92 @@
+var __root = __dirname + '/../..';
+var __apps = __root + '/apps';
+var __src = __root + '/src';
+var __js = __src + '/js';
+
+var fs = require( 'fs' );
+
+var app;
+var apps = [];
+
+module.exports = function( a ) {
+	// Grab the express app reference locally
+	app = a;
+
+	// Loop through main app director contents
+	loadApps();
+
+	// Load template locals;
+	app.use( require( __js + '/template-locals' )( apps ) );
+
+	// Route apps
+	routeApps();
+};
+
+function loadApps() {
+	var files = fs.readdirSync( __apps );
+	for ( var f in files ) {
+		// Only read directories
+		var file = __apps + '/' + files[f];
+		if ( fs.statSync( file ).isDirectory() ) {
+
+			// Check for a config.json file
+			var config_file = file + '/config.json';
+			if ( fs.existsSync( config_file ) ) {
+
+				// Parse the config into apps array
+				var output = JSON.parse( fs.readFileSync( config_file ) );
+				output.uid = files[f];
+				if ( output.priority === undefined ) output.priority = 100;
+				output.app = file + '/app.js';
+
+				// Check for sub apps directory
+				output.subapps = [];
+				var subapp_path = file + '/apps';
+				if ( fs.existsSync( subapp_path ) ) {
+					// Fetch the contents of the subapp directory
+					var subapps = fs.readdirSync( subapp_path );
+					for ( var a in subapps ) {
+						// Only read directories
+						var subapp = subapp_path + '/' + subapps[a];
+						if ( fs.statSync( subapp ).isDirectory() ) {
+							// Check for a config.json file
+							var sub_config_file = subapp + '/config.json';
+							if ( fs.existsSync( sub_config_file ) ) {
+								// Parse the config into apps array
+								var subapp_output = JSON.parse( fs.readFileSync ( sub_config_file ) );
+								subapp_output.uid = subapps[a];
+								if ( subapp_output.priority === undefined ) subapp_output.priority = 100;
+								subapp_output.app = subapp + '/app.js';
+								output.subapps.push( subapp_output );
+							}
+						}
+					}
+				}
+				output.subapps.sort( function( a, b ) {
+					return a.priority < b.priority;
+				} );
+				apps.push( output );
+			}
+		}
+	}
+	apps.sort( function( a, b ) {
+		return a.priority < b.priority;
+	} );
+}
+
+function routeApps() {
+	for ( var a in apps ) {
+		var _app = apps[a];
+		console.log( "	Route: /" + _app.path );
+		var new_app = require( _app.app )( _app );
+		app.use( '/' + _app.path, new_app );
+		if ( _app.subapps.length > 0 ) {
+			for ( var s in _app.subapps ) {
+				var _sapp = _app.subapps[s];
+				console.log( "	       /" + _app.path + "/" + _sapp.path  );
+				var new_sub_app = require( _sapp.app )( _sapp );
+				new_app.use( '/' + _sapp.path, new_sub_app );
+			}
+		}
+	}
+}

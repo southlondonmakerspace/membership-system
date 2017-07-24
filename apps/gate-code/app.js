95f56ea599eff4c001cc2059c4e9b3b076ae4f3d
Tom Lynch <me@unknowndomain.co.uk>
2017-07-24 23:36:36 +0100

    Fixes major crash

--- apps/gate-code/app.js
+++ apps/gate-code/app.js
@@ -32,7 +32,7 @@ app.use( function( req, res, next ) {
 app.get( '/', auth.isMember, function( req, res ) {
 	Options.findOne( { key: 'gate-code' }, function( err, option ) {
 		if ( ! option ) {
-			res.redirect( '/' );
+			return res.redirect( '/' );
 		}
 
 		res.render( 'index', { "code": option.value } );

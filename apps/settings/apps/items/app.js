0ab1465d716cd50e49ec8b1266293b851c7f3df6
Tom Lynch <me@unknowndomain.co.uk>
2017-12-28 17:20:07 +0000

    First stab at item, state and statuses

---
+++ apps/settings/apps/items/app.js
@@ -0,0 +1,121 @@
+var __root = '../../../..';
+var __src = __root + '/src';
+var __js = __src + '/js';
+var __config = __root + '/config';
+
+var	express = require( 'express' ),
+	app = express();
+
+var db = require( __js + '/database' ),
+	Items = db.Items,
+	States = db.States
+
+var auth = require( __js + '/authentication' );
+
+var config = require( __config + '/config.json' );
+
+var app_config = {};
+
+app.set( 'views', __dirname + '/views' );
+
+app.use( function( req, res, next ) {
+	res.locals.app = app_config;
+	res.locals.breadcrumb.push( {
+		name: app_config.title,
+		url: app.parent.mountpath + app.mountpath
+	} );
+	res.locals.activeApp = 'settings';
+	next();
+} );
+
+app.get( '/', auth.isSuperAdmin, function( req, res ) {
+	Items.find( function( err, items ) {
+		res.render( 'index', { items: items } );
+	} );
+} );
+
+app.get( '/create', auth.isSuperAdmin, function( req, res ) {
+	res.locals.breadcrumb.push( {
+		name: 'Create'
+	} );
+		States.find( function (err, states) {
+			res.render( 'create', { states: states } );
+		});
+
+} );
+
+app.post( '/create', auth.isSuperAdmin, function( req, res ) {
+	if ( ! req.body.name || req.body.name.trim() === '' ) {
+		req.flash( 'danger', 'item-name-required' );
+		res.redirect( app.parent.mountpath + app.mountpath );
+		return;
+	}
+
+	if ( ! req.body.slug || req.body.slug.trim() === '' ) {
+		req.flash( 'danger', 'item-slug-required' );
+		res.redirect( app.parent.mountpath + app.mountpath );
+		return;
+	}
+
+	var item = {
+		name: req.body.name,
+		slug: req.body.slug,
+		description: req.body.description,
+		guide: req.body.guide,
+		defaultState: req.body.defaultState
+	};
+
+	new Items( item ).save( function( err, item ) {
+		req.flash( 'success', 'item-created' );
+		res.redirect( app.parent.mountpath + app.mountpath );
+	} );
+} );
+
+app.get( '/:slug/edit', auth.isSuperAdmin, function( req, res ) {
+	Items.findOne( { slug: req.params.slug }).exec ( function( err, item ) {
+		if ( ! item ) {
+			req.flash( 'warning', 'item-404' );
+			res.redirect( app.parent.mountpath + app.mountpath);
+			return;
+		}
+
+		res.locals.breadcrumb.push( {
+			name: item.name
+		} );
+			States.find( function (err, states) {
+				res.render( 'edit', { item: item, states: states } );
+			} );
+	} );
+} );
+
+app.post( '/:slug/edit', auth.isSuperAdmin, function( req, res ) {
+	if ( ! req.body.name || req.body.name.trim() === '' ) {
+		req.flash( 'danger', 'item-name-required' );
+		res.redirect( app.parent.mountpath + app.mountpath );
+		return;
+	}
+
+	if ( ! req.body.slug || req.body.slug.trim() === '' ) {
+		req.flash( 'danger', 'item-slug-required' );
+		res.redirect( app.parent.mountpath + app.mountpath );
+		return;
+	}
+
+	var item = {
+		name: req.body.name,
+		slug: req.body.slug,
+		description: req.body.description,
+		guide: req.body.guide,
+		defaultState: req.body.defaultState
+	};
+
+	Items.update( { slug: req.params.slug }, item, function( status ) {
+		req.flash( 'success', 'item-updated' );
+		res.redirect( app.parent.mountpath + app.mountpath );
+	} );
+} );
+
+module.exports = function( config ) {
+	app_config = config;
+	return app;
+};
---
+++ apps/status/app.js
@@ -0,0 +1,42 @@
+var __root = '../..';
+var __src = __root + '/src';
+var __js = __src + '/js';
+var __config = __root + '/config';
+
+var __apps = __dirname + '/apps';
+
+var	fs = require( 'fs' ),
+	express = require( 'express' ),
+	app = express();
+
+var config = require( __config + '/config.json' );
+
+var apps = [];
+var app_config = {};
+
+var auth = require( __js + '/authentication' );
+
+var	db = require( __js + '/database' ),
+	Items = db.Items;
+
+app.set( 'views', __dirname + '/views' );
+
+app.use( function( req, res, next ) {
+	res.locals.app = app_config;
+	res.locals.breadcrumb.push( {
+		name: app_config.title
+	} );
+	res.locals.activeApp = app_config.uid;
+	next();
+} );
+
+app.get( '/', auth.isSuperAdmin, function( req, res ) {
+	Items.find().populate( 'defaultState' ).sort( { name: 1 } ).exec( function( err, items ) {
+		res.render( 'index', { items: items } );
+	} );
+} );
+
+module.exports = function( config ) {
+	app_config = config;
+	return app;
+};

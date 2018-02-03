3fd7ca7855db760ef7264063d39151fc654fb497
Chris Roberts <chris@naxxfish.eu>
2018-02-03 22:45:57 +0000

    Remove some unneeded code

--- apps/reports/apps/member-subscriptions/app.js
+++ apps/reports/apps/member-subscriptions/app.js
@@ -4,13 +4,9 @@ var __js = __src + '/js';
 var __config = __root + '/config';
 
 var	express = require( 'express' ),
-	app = express(),
-	discourse = require( __js + '/discourse' );
+	app = express()
 
 var Options = require( __js + '/options.js' )();
-
-var PostcodesIO = require( 'postcodesio-client' ),
-	postcodes = new PostcodesIO();
 
 var escapeStringRegexp = require( 'escape-string-regexp' );
 
@@ -18,12 +14,9 @@ var crypto = require( 'crypto' );
 
 var moment = require( 'moment' );
 var	db = require( __js + '/database' ),
-	Permissions = db.Permissions,
 	Members = db.Members,
-	Payments = db.Payments,
-	Events = db.Events;
+	Payments = db.Payments
 
-var Mail = require( __js + '/mail' );
 
 var auth = require( __js + '/authentication' );
 
@@ -44,63 +37,6 @@ app.use( function( req, res, next ) {
 } );
 
 app.get( '/', auth.isSuperAdmin, function( req, res ) {
-	Permissions.find( function( err, permissions ) {
-		var filter_permissions = [];
-
-		// If not admin or requesting active members only add member permission to filtering list
-		if ( ! ( auth.canSuperAdmin( req ) == true && req.query.show_inactive_members ) ) {
-			var member = permissions.filter( function( permission ) {
-				if ( permission.slug == config.permission.member ) return true;
-				return false;
-			} )[0];
-			filter_permissions.push( member );
-			permissions = permissions.filter( function( p ) {
-				if ( p.slug == config.permission.member ) return false;
-				return true;
-			} );
-		}
-
-		// If requested add custom permission to filtering list
-		var permission;
-		if ( req.query.permission ) {
-			permission = permissions.filter( function( permission ) {
-				if ( permission.slug == req.query.permission ) return true;
-				return false;
-			} );
-			if ( permission.length !== 1 ) {
-				permission = null;
-			} else if ( permission.length === 1 ) {
-				permission = permission[0];
-				filter_permissions.push( permission );
-				res.locals.breadcrumb.push( {
-					name: permission.name,
-				} );
-			}
-		}
-
-		// Add permission list to search parameters
-		var search = { $and: [] };
-		if ( filter_permissions.length > 0 ) {
-			var filter = [];
-			for ( var fp in filter_permissions ) {
-				filter.push( {
-					permissions: {
-						$elemMatch: {
-							permission: filter_permissions[fp]._id,
-							date_added: { $lte: new Date() },
-							$or: [
-								{ date_expires: null },
-								{ date_expires: { $gt: new Date() } }
-							]
-						}
-					}
-				} );
-			}
-			if ( filter != [] ) search['$and'] = filter;
-		}
-
-		var path = {};
-		if ( search['$and'].length == 0 ) search = {};
 
 		// Process pagination
 		var limit = 10;
@@ -112,7 +48,7 @@ app.get( '/', auth.isSuperAdmin, function( req, res ) {
 			page = parseInt( req.query.page );
 
 		// Perform search
-		Members.count( search, function( err, total ) {
+		Members.count( , function( err, total ) {
 			if ( req.query.show_inactive_members ) path.show_inactive_members = 'show_inactive_members=true';
 			if ( req.query.limit && req.query.limit > 0 && req.query.limit <= 1000 ) path.limit = 'limit=' + limit;
 			if ( req.query.page && req.query.page > 0 ) path.page = 'page=' + page;
@@ -143,41 +79,6 @@ app.get( '/', auth.isSuperAdmin, function( req, res ) {
 				next: next,
 				total: pages.length
 			};
-
-			// Limit
-			append_path = [];
-			Object.keys( path ).forEach( function( key ) {
-				if ( key == 'limit' ) return;
-				if ( key == 'page' ) return;
-				append_path.push( path[key] );
-			} );
-			append_path = append_path.join( '&' );
-
-			var limits = [ 10, 25, 50, 100, 250, 500, 1000 ];
-			limits.forEach( function( limit, l ) {
-				limits[l] = {
-					number: limit,
-					path: '?limit=' + limit + ( append_path ? '&' + append_path : '' )
-				}
-			} );
-
-			// Inactive members
-			append_path = [];
-			Object.keys( path ).forEach( function( key ) {
-				if ( key == 'show_inactive_members' ) return;
-				if ( key == 'page' ) return;
-				append_path.push( path[key] );
-			} );
-
-/*,
-{
-	"$group": {
-		"_id": "$_id",
-		"lastPayment": {
-			"$last":"$payment"
-		}
-	}
-}*/
 
 			Members.aggregate([
 				{
@@ -238,7 +139,7 @@ app.get( '/', auth.isSuperAdmin, function( req, res ) {
 				} );
 			} );
 		} );
-	} );
+
 } );
 
 

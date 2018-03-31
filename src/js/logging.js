7696efa095610c68fedfcb7835ccfa13eb5494c7
Chris Roberts <1645367+naxxfish@users.noreply.github.com>
2018-03-31 16:12:31 +0100

    Added anonymised session ID and user id to logs by default

--- src/js/logging.js
+++ src/js/logging.js
@@ -11,6 +11,9 @@ var bunyan = require( 'bunyan' ),
 	bunyanMiddleware = require( 'bunyan-middleware' ),
 	SyslogStream = require( 'bunyan-syslog-unixdgram' )
 
+var crypto = require('crypto')
+var hash = crypto.createHash;
+var randomKey = crypto.randomBytes(256);
 
 // Bunyan logging
 var bunyanConfig = {
@@ -72,8 +75,27 @@ if (config.syslog == true) {
 
 function loggingMiddleware(req, res, next) {
 	var log = req.log;
-	function logAThing( level, params )
+	function logAThing( level, params, req )
 	{
+		params.ip = req.connection.remoteAddress; //TODO: this will only be correct when behind a reverse proxy, if app.set('trust proxy') is enabled!
+		if (! params.sensitive )
+		{
+			params.sensitive = {};
+		}
+		if ( req.user ) {
+			params.sensitive.user = {
+				uuid: req.user.uuid,
+				firstname: req.user.firstname,
+				lastname: req.user.lastname,
+				email: req.user.email
+			};
+			params.anon_userid = hash('sha1').update(req.user.uuid + randomKey).digest('base64');
+		}
+		if ( req.sessionID )
+		{
+			params.sensitive.sessionID = req.sessionID;
+			params.anon_sessionId = hash('sha1').update(req.sessionID + randomKey).digest('base64');
+		}
 		if (params.sensitive)
 		{
 			log[level](params);
@@ -85,19 +107,19 @@ function loggingMiddleware(req, res, next) {
 	req.log = {
 		info: function (params)
 		{
-			logAThing( 'info', params );
+			logAThing( 'info', params , req );
 		},
 		debug: function (params)
 		{
-			logAThing( 'debug', params );
+			logAThing( 'debug', params , req );
 		},
 		error: function (params)
 		{
-			logAThing( 'error', params );
+			logAThing( 'error', params , req );
 		},
 		fatal: function (params)
 		{
-			logAThing( 'fatal', params );
+			logAThing( 'fatal', params , req );
 		}
 	}
 	next();

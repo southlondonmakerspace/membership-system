8231a8b0f7ad2b6ef97905cbb7313a983daeddf9
Will Franklin <will@willpf.co.uk>
2018-07-04 10:14:57 +0100

    Log requests and errors to mailchimp

--- src/js/mailchimp.js
+++ src/js/mailchimp.js
@@ -2,14 +2,43 @@ const axios = require('axios');
 const crypto = require('crypto');
 const config = require('../../config/config.json');
 
+const { log } = require('./logging');
+
 function createInstance(endpoint) {
-	return axios.create({
+	const instance = axios.create({
 		baseURL: `https://${config.mailchimp.datacenter}.api.mailchimp.com/3.0${endpoint}`,
 		auth: {
 			username: 'user',
 			password: config.mailchimp.api_key
 		}
 	});
+
+	instance.interceptors.request.use(config => {
+		log.debug({
+			app: 'mailchimp' + endpoint,
+			url: config.url,
+			method: config.method,
+			sensitive: {
+				params: config.params,
+				data: config.data
+			}
+		});
+
+		return config;
+	});
+
+	instance.interceptors.response.use(response => {
+		return response;
+	}, error => {
+		log.error({
+			app: 'mailchimp' + endpoint,
+			status: error.response.status,
+			data: error.response.data
+		});
+		return Promise.reject(error);
+	});
+
+	return instance;
 }
 
 function emailToHash(email) {
@@ -44,6 +73,7 @@ function lists(listId) {
 const batchInstance = createInstance('/batches');
 
 module.exports = {
+	instance: createInstance(''),
 	lists,
 	batches: {
 		async create(operations) {

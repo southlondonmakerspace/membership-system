12c2fae96ec3b8d56af152814a43e3e97824fcd0
Will Franklin <will@willpf.co.uk>
2018-07-04 10:49:05 +0100

    Add Mandrill and tidy password reset

--- apps/password-reset/app.js
+++ apps/password-reset/app.js
@@ -1,22 +1,23 @@
 var __root = '../..';
 var __src = __root + '/src';
 var __js = __src + '/js';
-var __config = __root + '/config';
 
 var	express = require( 'express' ),
 	app = express();
 
-var Mail = require( __js + '/mail' );
-
 var	Members = require( __js + '/database' ).Members;
 
-var auth = require( __js + '/authentication' );
+const { wrapAsync } = require( __js + '/utils' );
+const { hasSchema } = require( __js + '/middleware' );
+const mandrill = require( __js + '/mandrill' );
 
-var config = require( __config + '/config.json' );
+var auth = require( __js + '/authentication' );
 
 var app_config = {};
 
 app.set( 'views', __dirname + '/views' );
+
+app.use( auth.isNotLoggedIn );
 
 app.use( function( req, res, next ) {
 	res.locals.app = app_config;
@@ -27,41 +28,33 @@ app.get( '/' , function( req, res ) {
 	res.render( 'index' );
 } );
 
-app.post( '/', function( req, res ) {
+const getResetCodeSchema = {
+	body: {
+		type: 'object',
+		required: ['email'],
+		properties: {
+			email: {
+				type: 'string',
+				format: 'email'
+			}
+		}
+	}
+};
+
+app.post( '/', hasSchema(getResetCodeSchema).orFlash, wrapAsync( async function( req, res ) {
 	if ( ! req.body.email ) {
 		req.flash( 'danger', 'information-ommited' );
 		res.redirect( app.mountpath );
 		return;
 	}
-	Members.findOne( { email: req.body.email }, function( err, user ) {
-		if ( user ) {
-			auth.generateActivationCode( function( code ) {
-				var password_reset_code = code;
-				user.password.reset_code = password_reset_code;
-				user.save( function() {} );
+	const member = await Members.findOne( { email: req.body.email } );
 
-				var options = {
-					firstname: user.firstname,
-					reset_url: config.audience + '/password-reset/code/' + password_reset_code
-				};
+	const code = auth.generateCode();
+	member.password.reset_code = code;
+	await member.save();
 
-				Mail.sendMail(
-					req.body.email,
-					'Password Reset',
-					__dirname + '/email-templates/reset.text.pug',
-					__dirname + '/email-templates/reset.html.pug',
-					options,
-					function() {
-						req.flash( 'success', 'password-reset' );
-						res.redirect( app.mountpath );
-					} );
-			} );
-		} else {
-			req.flash( 'success', 'password-reset' );
-			res.redirect( app.mountpath );
-		}
-	} );
-} );
+	await mandrill.send('reset-password', member);
+} ) );
 
 app.get( '/code', function( req, res ) {
 	res.render( 'change-password' );
@@ -71,47 +64,44 @@ app.get( '/code/:password_reset_code', function( req, res ) {
 	res.render( 'change-password', { password_reset_code: req.params.password_reset_code } );
 } );
 
-app.post( '/change-password', function( req, res ) {
-	if ( ! req.body.password_reset_code ) {
-		req.flash( 'danger', 'information-ommited' );
-		res.redirect( app.mountpath );
-		return;
-	}
-	Members.findOne( { 'password.reset_code': req.body.password_reset_code }, function( err, user ) {
-		if ( user ) {
-			if ( req.body.password != req.body.verify ) {
-				req.flash( 'danger', 'password-err-mismatch' );
-				res.redirect( app.mountpath + '/code/' + req.body.password_reset_code );
-				return;
+const resetPasswordSchema = {
+	body: {
+		type: 'object',
+		required: ['password_reset_code', 'password', 'verify'],
+		properties: {
+			password_reset_code: {
+				type: 'string'
+			},
+			password: {
+				type: 'string',
+				format: 'password'
+			},
+			verify: {
+				const: {
+					'$data': '1/password'
+				}
 			}
-
-			var passwordRequirements = auth.passwordRequirements( req.body.password );
-			if ( passwordRequirements !== true ) {
-				req.flash( 'danger', passwordRequirements );
-				res.redirect( app.mountpath + '/code/' + req.body.password_reset_code );
-				return;
-			}
-
-			auth.generatePassword( req.body.password, function( password ) {
-				Members.update( { _id: user._id }, { $set: {
-					'password.salt': password.salt,
-					'password.hash': password.hash,
-					'password.reset_code': null,
-					'password.tries': 0,
-					'password.iterations': password.iterations
-				} }, function() {
-					req.login( user, function( ) {
-						req.flash( 'success', 'password-changed' );
-						res.redirect( '/' );
-					} );
-				} );
-			} );
-		} else {
-			req.flash( 'danger', 'password-reset-code-err' );
-			res.redirect( '/login' );
 		}
+	}
+};
+
+app.post( '/change-password', hasSchema(resetPasswordSchema).orFlash, wrapAsync( async function( req, res ) {
+	const member = await Members.findOne( { 'password.reset_code': req.body.password_reset_code } );
+	const password = await auth.generatePasswordPromise( req.body.password );
+
+	await member.update( { $set: {
+		'password.salt': password.salt,
+		'password.hash': password.hash,
+		'password.reset_code': null,
+		'password.tries': 0,
+		'password.iterations': password.iterations
+	} } );
+
+	req.login( member, function( ) {
+		req.flash( 'success', 'password-changed' );
+		res.redirect( '/' );
 	} );
-} );
+} ) );
 
 module.exports = function( config ) {
 	app_config = config;

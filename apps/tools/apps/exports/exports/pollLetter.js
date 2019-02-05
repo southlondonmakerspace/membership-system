ea0da48f0afda5ace3c310c4f31de516ba8acc4a
Will Franklin <will@willpf.co.uk>
2019-02-05 16:32:20 +0000

    Filter address fields

--- apps/tools/apps/exports/exports/pollLetter.js
+++ apps/tools/apps/exports/exports/pollLetter.js
@@ -19,16 +19,22 @@ async function getQuery() {
 }
 
 async function getExport(members) {
-	return members.map(member => ({
-		'First name': member.firstname,
-		Surname: member.lastname,
-		'Full name': member.fullname,
-		Address1: member.delivery_address.line1,
-		Address2: member.delivery_address.line2,
-		Address3: member.delivery_address.city,
-		Address4: member.delivery_address.postcode,
-		'Custom 1': 'thebristolcable.org/vote2019/' + member.pollsCode
-	}));
+	return members.map(member => {
+		const addressFields = Object.assign(
+			...['line1', 'line2', 'city', 'postcode']
+				.map(field => member.delivery_address[field])
+				.filter(line => !!line)
+				.map((field, i) => ({['Address' + (i + 1)]: field}))
+		);
+
+		return {
+			'First name': member.firstname,
+			Surname: member.lastname,
+			'Full name': member.fullname,
+			'Custom 1': 'thebristolcable.org/vote2019/' + member.pollsCode,
+			...addressFields
+		};
+	});
 }
 
 module.exports = {

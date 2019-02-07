b5397230d1c302c4cba8624c9665faa7ebd0171f
Will Franklin <will@willpf.co.uk>
2019-02-07 14:29:08 +0000

    Upgrade packages

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,23 @@ const postcss = require('gulp-postcss');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('css', () => {
+function css() {
 	return gulp.src('./static/scss/**/*.scss')
 		.pipe(sourcemaps.init())
 		.pipe(sass().on('error', sass.logError))
 		.pipe(postcss([autoprefixer()]))
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('./static/css'));
-});
+}
 
-gulp.task('build', ['css']);
+const build = gulp.parallel(css);
 
-gulp.task('watch', () => {
-	gulp.watch('./static/scss/**/*.scss', ['css']);
-});
+function watch() {
+	gulp.watch('./static/scss/**/*.scss', css);
+}
+
+module.exports = {
+	default: gulp.series(build, watch),
+	build,
+	watch
+};

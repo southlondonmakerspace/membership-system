b9fde51ed5c2e1e619d57e8309be49c86ab8c8bd
Chris Roberts <chris@naxxfish.eu>
2020-01-31 18:41:14 +0000

    proper CSV generation

--- tools/migrate-to-woo.js
+++ tools/migrate-to-woo.js
@@ -4,8 +4,8 @@ var __src = __root + '/src';
 var __js = __src + '/js';
 const daysItTakesToProcessPayments = 2;
 const moment = require('moment')
-const wooSubscriptionProductId = 13;
 const util = require('util');
+const stringify = require('csv-stringify')
 
 var config = require(__config),
     db = require(__js + '/database').connect(config.mongo),
@@ -38,6 +38,7 @@ const columns = [
     "customer_note",
     "_old_gocardless_subscription_amount",
     "_old_gocardless_subscription_id",
+    "_old_gocardless_mandate_id",
     "_old_permissions"
 ];
 
@@ -51,7 +52,6 @@ function doubleSquashObject(anArray) {
 }
 
 function squashObject(anArray) {
-    const aStringValue = ""
     const stringParts = []
     Object.keys(anArray).forEach((key) => {
         if (typeof anArray[key] === 'object') {
@@ -63,16 +63,12 @@ function squashObject(anArray) {
     return stringParts.join('|');
 }
 
-function headerRow() {
-    return columns.join(',');
-}
-
-function memberRow(flatMember) {
-    var row = []
-    columns.forEach(function (column) {
-        row.push(flatMember[column]);
+function flattenPermissions(permissions) {
+    const permissionsArray = [];
+    permissions.forEach((permission) => {
+        permissionsArray.push( permission.permission.toString() );
     })
-    return row.join(',');
+    return squashObject(permissionsArray);
 }
 
 function flattenMember(member) {
@@ -81,60 +77,47 @@ function flattenMember(member) {
         "customer_email": member.email,
         "billing_first_name": member.firstname,
         "billing_last_name": member.lastname,
-        "billing_address_1": `"${address1.replace('\n', '')}"`,
-        "billing_address_2": `"${address2.replace('\n', '')}"`,
-        "billing_postcode": `"${postcode.replace('\n', '')}"`,
+        "billing_address_1": `${address1.replace('\n', '')}`,
+        "billing_address_2": `${address2.replace('\n', '')}`,
+        "billing_postcode": `${postcode.replace('\n', '')}`,
         "shipping_first_name": member.firstname,
         "shipping_last_name": member.lastname,
-        "shipping_address_1": `"${address1.replace('\n', '')}"`,
-        "shipping_address_2": `"${address2.replace('\n', '')}"`,
-        "shipping_postcode": `"${postcode.replace('\n', '')}"`,
+        "shipping_address_1": `${address1.replace('\n', '')}`,
+        "shipping_address_2": `${address2.replace('\n', '')}`,
+        "shipping_postcode": `${postcode.replace('\n', '')}`,
         "subscription_status": 'wc-active',
         "start_date": moment(member.joined).format('YYYY-MM-DD HH:mm:ss'),
         "next_payment_date": moment(member.gocardless.next_possible_charge_date).format('YYYY-MM-DD HH:mm:ss'),
-        "billing_period": "month",
-        "billing_interval": 1,
-        "order_items": squashObject({
-            product_id: wooSubscriptionProductId,
-            quantity: 1,
-            total: member.gocardless.amount,
-            tax: 0
-        }),
         "payment_method": "gocardless",
         "payment_method_title": "Direct Debit",
-        "payment_method_post_meta": squashObject({
-            "_gocardless_mandate_id": member.gocardless.mandate_id,
-            "_gocardless_mandate":
-            {
-                id: member.gocardless.mandate_id
-            }
-        }),
         "customer_note": "Migrated from SLMS Membership System 2020",
         "_old_gocardless_subscription_amount": member.gocardless.amount,
         "_old_gocardless_subscription_id": member.gocardless.subscription_id,
-        "_old_permissions": member.permissions.join(",")
+        "_old_gocardless_mandate_id": member.gocardless.mandate_id,
+        "_old_permissions": flattenPermissions(member.permissions)
     };
 
 }
-console.log(headerRow());
 Members.find({
     'gocardless.subscription_id': { $exists: true }
-}, function (err, members) {
-    members.forEach(async function (member) {
-        const nextChargeDate = moment(member.gocardless.next_possible_charge_date);
+}, async function (err, members) {
+    const records = [];
+    for (index = 0, len = members.length; index < len; ++index) {
+        /*const nextChargeDate = moment(member.gocardless.next_possible_charge_date);
         if (nextChargeDate.isBefore(paymentCutoffDate)) {
             return;
-        }
+        }*/
+        const member = members[index];
         try {
-            const subscription = await util.promisify(GoCardless.getSubscription)(member.gocardless.subscription_id);
-            if (subscription.status === 'active') {
-                const flatMember = flattenMember(member);
-                console.log(memberRow(flatMember));
-            } else {
-                return;
-            }
+            records.push( flattenMember(member) )
         } catch (e) {
             console.error(e);
         }
-    });
+    }
+    stringify(records, {
+        header: true,
+        columns
+    }, function (err, data) {
+        console.log(data);
+    })
 });

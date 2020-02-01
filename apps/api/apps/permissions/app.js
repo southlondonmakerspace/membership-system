306072d88cbb9e00e586a5d932692f83d32595b5
Chris Roberts <chris@naxxfish.net>
2020-02-01 19:40:04 +0000

    Added support for an external permissions validator for dual running

--- apps/api/apps/permissions/app.js
+++ apps/api/apps/permissions/app.js
@@ -17,49 +17,69 @@ var database = require( __js + '/database' ),
 	Permissions = database.Permissions,
 	Events = database.Events;
 
+var externalValidator = require('./external-validator.js');	
 var app_config = {};
 
 app.get( '/:slug/:tag', auth.isAPIAuthenticated, function( req, res ) {
-	Members.findOne( { 'tag.hashed': req.params.tag } ).populate( 'permissions.permission' ).exec( function( err, member ) {
-		var grantAccess = false;
-		if ( member ) {
-			var hasMembership = false;
-			var hasPermission = false;
-			var isSuperAdmin = false;
-
-			for ( var p = 0; p < member.permissions.length; p++ ) {
-				var permission = member.permissions[p];
-				if ( permission.permission.slug == config.permission.superadmin && permission.date_added <= new Date() && ( ! permission.date_expires || permission.date_expires > new Date() ) ) isSuperAdmin = true;
-				if ( permission.permission.slug == config.permission.member && permission.date_added <= new Date() && ( ! permission.date_expires || permission.date_expires > new Date() ) ) hasMembership = true;
-				if ( permission.permission.slug == req.params.slug && permission.date_added <= new Date() && ( ! permission.date_expires || permission.date_expires > new Date() ) ) hasPermission = true;
-			}
-
-			if ( ( isSuperAdmin && hasPermission ) || ( hasMembership && hasPermission ) ) {
-				Permissions.findOne( { slug: req.params.slug }, function ( err, permission ) {
-					new Events( {
-						member: member._id,
-						permission: permission._id,
-						successful: true
-					} ).save( function( status ) {} );
-				} );
-				res.send( JSON.stringify( {
-					name: member.fullname
-				} ) );
+	var externalValidatorEnabled = Options.getBool('external-permission-validator-enabled');
+	if (externalValidatorEnabled === true) {
+		var externalValidatorUrl = Options.getText('external-permission-validator-url');
+		externalValidator.validate({
+			slug: req.params.slug,
+			tag: req.params.tag,
+			url: externalValidatorUrl
+		}, function (err, result) {
+			if (err) {
+				res.status(403).send({error: `${err}`});
+				return;
 			} else {
-				res.sendStatus( 403 );
-				Permissions.findOne( { slug: req.params.slug }, function ( err, permission ) {
-					if ( permission )
+				res.status(200).send(result);
+				return;
+			}
+			res.status(500).send({error: 'External validator enabled, it didn\'t work'});
+		});
+	} else {
+		Members.findOne( { 'tag.hashed': req.params.tag } ).populate( 'permissions.permission' ).exec( function( err, member ) {
+			var grantAccess = false;
+			if ( member ) {
+				var hasMembership = false;
+				var hasPermission = false;
+				var isSuperAdmin = false;
+	
+				for ( var p = 0; p < member.permissions.length; p++ ) {
+					var permission = member.permissions[p];
+					if ( permission.permission.slug == config.permission.superadmin && permission.date_added <= new Date() && ( ! permission.date_expires || permission.date_expires > new Date() ) ) isSuperAdmin = true;
+					if ( permission.permission.slug == config.permission.member && permission.date_added <= new Date() && ( ! permission.date_expires || permission.date_expires > new Date() ) ) hasMembership = true;
+					if ( permission.permission.slug == req.params.slug && permission.date_added <= new Date() && ( ! permission.date_expires || permission.date_expires > new Date() ) ) hasPermission = true;
+				}
+	
+				if ( ( isSuperAdmin && hasPermission ) || ( hasMembership && hasPermission ) ) {
+					Permissions.findOne( { slug: req.params.slug }, function ( err, permission ) {
 						new Events( {
 							member: member._id,
 							permission: permission._id,
-							successful: false
+							successful: true
 						} ).save( function( status ) {} );
-				} );
+					} );
+					res.send( JSON.stringify( {
+						name: member.fullname
+					} ) );
+				} else {
+					res.sendStatus( 403 );
+					Permissions.findOne( { slug: req.params.slug }, function ( err, permission ) {
+						if ( permission )
+							new Events( {
+								member: member._id,
+								permission: permission._id,
+								successful: false
+							} ).save( function( status ) {} );
+					} );
+				}
+			} else {
+				res.sendStatus( 404 );
 			}
-		} else {
-			res.sendStatus( 404 );
-		}
-	} );
+		} );
+	}
 } );
 
 module.exports = function( config ) {
